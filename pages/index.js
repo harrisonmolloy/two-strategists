b8c8ce3feb754a189b0ccb0a89fe7574fb812a70
Harrison Molloy <hello@harrisonmolloy.com>
2020-09-29 18:06:17 +1300

    changes from first round of feedback

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,7 @@ export default function Home() {
       </Head>
 
       <header>
-        <a href="/">+64 (0)21 8403 3984</a>
-        <a href="/" class="title">Two Strategists</a>
+        <a href="/" className="title">Two Strategists</a>
         <a href="/">info@twostrategists.nz</a>
       </header>
 
@@ -22,60 +21,66 @@ export default function Home() {
         </section>
 
         <section>
-          <h1>Two strategists – one brand,<br/> one culture.</h1>
-          <h3>Working together to redefine your brand and culture through a meaningful purpose.</h3>
+          <h2>Working together to redefine your brand and culture through a meaningful purpose.</h2>
+          <h3>Two strategists - one brand, one culture.</h3>
+
         </section>
 
         <section>
-          <h1>A proven process.</h1>
-          <h3>Flexible. Fast. Effective. →</h3>
+          <h1>Flexible. Fast. Effective.</h1>
+          <h3>A proven process. →</h3>
         </section>
 
         <section>
-          <h2>→ Review</h2>
-          <p>We review your current brand and culture. We listen, read, have conversations and together we work out what’s needed for your business.</p>
+          <h2>Review</h2>
+          <h3>We review your current brand and culture.</h3>
+          <h4>We listen, read, have conversations and together we work out what’s needed for your business.</h4>
         </section>
 
         <section>
-          <h2>→ Define</h2>
-          <p>We create a purposeful strategy and narrative. Defining the foundations of your brand and culture from the inside out — to drive business growth.</p>
-          {/* DNA strand */}
-          <p>We start with understanding what you stand, what draws you together and what’s driving you forward. These are the building blocks to creating an aligned brand and culture strategy.</p>
+          <h2>Define</h2>
+          <h3>We create a purposeful strategy and narrative.</h3>
+          <h4>Defining the foundations of your brand and culture from the inside out — to drive business growth.</h4>
         </section>
 
         <section>
-          <h2>→ Embed</h2>
-          <p>We develop a roadmap. We make it easy for you to activate the strategy fast and effectively with clear priorities.</p>
-        </section>
-
-        <section class="about">
-          <div class="profile">
-            <h3>Hilary Cootes</h3>
-            <p>Brand strategist.<br/>
-            20 years. 7 agencies. 4 countries.<br/>
-            Large international brands and brands for proud local owners.<br/>
-            <a href="https://hilarycootes.com/">hilarycootes.com</a>
-            </p>
-          </div>
-
-          <div class="profile">
-            <h3>Kath Knight</h3>
-            <p>Culture strategist.<br/>
-            20 years. Ex agency owner.<br/>
-            Government. FMCG. Energy. Insurance. Finance<br/>
-            <a href="/">LinkedIn</a>
-            </p>
-          </div>
+          <img src="DNA strand"/>
+          <h4>We start with understanding what you stand for, what draws you together and what’s driving you forward. These are the building blocks to creating an aligned brand and culture strategy.</h4>
         </section>
 
         <section>
+          <h2>Embed</h2>
+          <h3>We develop a roadmap.</h3>
+          <p>We make it easy for you to activate the strategy fast and effectively with clear priorities.</p>
+        </section>
+
+        <section className="about">
           <h2>Together.</h2>
-          <p>
-          <a href="/">The Warehouse Group (quote from Fiona H?)</a>
-          <a href="/">ACC (quote from Shaun ?)</a>
-          <a href="/">IANZ (quote from Elizabeth?)</a>
-          </p>
-          <p>Our success is measured in your people’s engagement lift and your overall business performance.(do we need this?)</p>
+
+          <div className="profile">
+            <h3>Hilary Cootes</h3>
+            <h4>Brand strategist.</h4>
+            <p>20 years. 7 agencies. 4 countries.
+            Large international brands and brands for proud local owners.
+            </p>
+            <a href="https://hilarycootes.com/">hilarycootes.com</a>
+          </div>
+
+          <div className="profile">
+            <h3>Kath Knight</h3>
+            <h4>Culture strategist.</h4>
+            <p>20 years. Ex agency owner.
+            Government. FMCG. Energy. Insurance. Finance
+            </p>
+            <a href="/">LinkedIn</a>
+          </div>
+
+          <div className="profile">
+            {/*<a href="/">The Warehouse Group (quote from Fiona H?)</a>
+            <a href="/">ACC (quote from Shaun ?)</a>
+            <a href="/">IANZ (quote from Elizabeth?)</a>*/}
+            <p>Our success is measured in your people’s engagement lift and your overall business performance.(do we need this?)</p>
+          </div>
         </section>
 
         <section>
