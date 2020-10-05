6c4bbe886f85e1f13bd4b6c4359d9163f045a289
Harrison Molloy <hello@harrisonmolloy.com>
2020-10-05 17:52:47 +1300

    resetting

--- pages/index.js
+++ pages/index.js
@@ -10,85 +10,81 @@ export default function Home() {
       </Head>
 
       <header>
-        <a href="/" className="title">Two Strategists</a>
-        <a href="/" className="email">info@twostrategists.nz</a>
+        <ul>
+          <li><a href="/" className="title">Two Strategists</a></li>
+          <li><a href="/" className="email">info@twostrategists.nz</a></li>
+        </ul>
       </header>
 
-      <main>
-        <section>
-          <h1 >Who are you in the new economy?</h1>
-          <h3>We can help you with that.</h3>
-        </section>
+      <section>
+        <h1>Who are you in the new economy?</h1>
+        <h2>We can help you with that.</h2>
+      </section>
 
-        <section>
-          <h2>Working together to redefine your brand and culture through a meaningful purpose.</h2>
-          <h3>Two strategists - one brand, one culture.</h3>
+      <section>
+        <h1>Two strategists - one brand, one culture.</h1>
+        <h2>Working together to redefine your brand and culture through a meaningful purpose.</h2>
+      </section>
 
-        </section>
+      <section className="process">
+        <h1>Flexible. Fast. Effective.</h1>
+        <h2>A proven process.</h2>
+        <ul>
+          <li>
+            <h2>Review</h2>
+            <h3>We review your current brand and culture.</h3>
+            <p>We listen, read, have conversations and together we work out what’s needed for your business.</p>
+          </li>
+          <li>
+            <h2>Define</h2>
+            <h3>We create a purposeful strategy and narrative.</h3>
+            <p>Defining the foundations of your brand and culture from the inside out — to drive business growth.</p>
+            <p>We start with understanding what you stand for, what draws you together and what’s driving you forward. These are the building blocks to creating an aligned brand and culture strategy.</p>
+          </li>
+          <li>
+            <h2>Embed</h2>
+            <h3>We develop a roadmap.</h3>
+            <p>We make it easy for you to activate the strategy fast and effectively with clear priorities.</p>
+          </li>
+        </ul>
+      </section>
 
-        <section>
-          <h1>Flexible. Fast. Effective.</h1>
-          <h3>A proven process.</h3>
-        </section>
-
-        <section>
-          <h2>Review</h2>
-          <h3>We review your current brand and culture.</h3>
-          <h4>We listen, read, have conversations and together we work out what’s needed for your business.</h4>
-        </section>
-
-        <section>
-          <h2>Define</h2>
-          <h3>We create a purposeful strategy and narrative.</h3>
-          <h4>Defining the foundations of your brand and culture from the inside out — to drive business growth.</h4>
-          <h4>We start with understanding what you stand for, what draws you together and what’s driving you forward. These are the building blocks to creating an aligned brand and culture strategy.</h4>
-        </section>
-
-        <section>
-          <h2>Embed</h2>
-          <h3>We develop a roadmap.</h3>
-          <p>We make it easy for you to activate the strategy fast and effectively with clear priorities.</p>
-        </section>
-
-        <section className="about">
-          <h2>Together.</h2>
-
-          <div className="profile">
+      <section className="about">
+        <ul>
+          <li>
             <h3>Hilary Cootes</h3>
             <h4>Brand strategist.</h4>
             <p>20 years. 7 agencies. 4 countries.<br/>
             Large international brands and brands for proud local owners.
             </p>
             <a href="https://hilarycootes.com/">hilarycootes.com</a>
-          </div>
-
-          <div className="profile">
+          </li>
+          <li>
             <h3>Kath Knight</h3>
             <h4>Culture strategist.</h4>
             <p>20 years. Ex agency owner.<br/>
             Government. FMCG. Energy. Insurance. Finance
             </p>
             <a href="/">LinkedIn</a>
-          </div>
-
-          {/*
-          <div className="profile">
-            <a href="/">The Warehouse Group (quote from Fiona H?)</a>
-            <a href="/">ACC (quote from Shaun ?)</a>
-            <a href="/">IANZ (quote from Elizabeth?)</a>
+          </li>
+          <li>
+            <h2>Together.</h2>
+            <ul>
+              <li><a href="/">The Warehouse Group</a></li>
+              <li><a href="/">ACC</a></li>
+              <li><a href="/">IANZ</a></li>
+            </ul>
             <p>Our success is measured in your people’s engagement lift and your overall business performance.</p>
-            (do we need this?)
-          </div>
-          */}
-        </section>
+          </li>
+        </ul>
+      </section>
 
-        <section>
-          <h2><a href="/">Contact us →</a></h2>
-        </section>
-      </main>
+      <section>
+        <h2><a href="/">Contact us →</a></h2>
+      </section>
 
       <footer>
-        {/* copyright two strategists 2020 */}
+        <p>copyright two strategists 2020</p>
       </footer>
     </>
   )
