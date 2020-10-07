11c5fdc70917c89497c744213215c0bf9a1cb486
Harrison Molloy <hello@harrisonmolloy.com>
2020-10-08 12:43:11 +1300

    adding prettier, simplifying

--- assets/copy.md
+++ assets/copy.md
@@ -3,19 +3,23 @@ twostrategists.nz
 twostrategists.com
 
 # Who are you in the new economy?
+
 We can help you with that.
 
 # Two strategists – one brand, one culture.
+
 Working together to redefine your brand and culture through a meaningful purpose.
 
 A proven process.
 Flexible. Fast. Effective.
 
 ## REVIEW
+
 We review your current brand and culture
 We listen, read, have conversations and together we work out what’s needed for your business.
 
 ## DEFINE
+
 We create a purposeful strategy and narrative
 Defining the foundations of your brand and culture from the inside out — to drive business growth.
 
@@ -24,22 +28,26 @@ We start with understanding what you stand, what draws you together and what’s
 These are the building blocks to creating an aligned brand and culture strategy.
 
 ## EMBED
+
 We develop a roadmap
 We make it easy for you to activate the strategy fast and effectively with clear priorities.
 
 ## Hilary Cootes
+
 Brand strategist.
 20 years. 7 agencies. 4 countries.
 Large international brands and brands for proud local owners.
 (link to Hils website – hilarycootes.com)
 
 ## Kath Knight
+
 Culture strategist.
 20 years. Ex agency owner.
 Government. FMCG. Energy. Insurance. Finance
 (link to my LinkedIn?)
 
 ## Together.
+
 The Warehouse Group (quote from Fiona H?)
 ACC (quote from Shaun ?)
 IANZ (quote from Elizabeth?)
@@ -48,4 +56,5 @@ IANZ (quote from Elizabeth?)
 Our success is measured in your people’s engagement lift and your overall business performance.
 
 ## CONTACT US
+
 links
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -8,11 +8,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
       <header className="grid">
         <ul>
-          <li><a href="/" className="title">Two Strategists</a></li>
-          <li><a href="/" className="email">info@twostrategists.nz</a></li>
+          <li>
+            <a href="/" className="title">
+              Two Strategists
+            </a>
+          </li>
+          <li>
+            <a href="/" className="email">
+              info@twostrategists.nz
+            </a>
+          </li>
         </ul>
       </header>
 
@@ -24,7 +31,10 @@ export default function Home() {
 
         <section>
           <h1>Two strategists - one brand, one culture.</h1>
-          <p class="subtitle">Working together to redefine your brand and culture through a meaningful purpose.</p>
+          <p class="subtitle">
+            Working together to redefine your brand and culture through a
+            meaningful purpose.
+          </p>
         </section>
 
         <section className="process">
@@ -34,20 +44,33 @@ export default function Home() {
             <li>
               <h2>Review</h2>
               <p class="subtitle">We review your current brand and culture.</p>
-              <p>We listen, read, have conversations and together we work out what’s needed for your business.</p>
+              <p>
+                We listen, read, have conversations and together we work out
+                what’s needed for your business.
+              </p>
             </li>
             <li>
               <h2>Define</h2>
               <p class="subtitle">
                 We create a purposeful strategy and narrative.
               </p>
-              <p>Defining the foundations of your brand and culture from the inside out — to drive business growth.</p>
-              <p>We start with understanding what you stand for, what draws you together and what’s driving you forward. These are the building blocks to creating an aligned brand and culture strategy.</p>
+              <p>
+                Defining the foundations of your brand and culture from the
+                inside out — to drive business growth.
+              </p>
+              <p>
+                We start with understanding what you stand for, what draws you
+                together and what’s driving you forward. These are the building
+                blocks to creating an aligned brand and culture strategy.
+              </p>
             </li>
             <li>
               <h2>Embed</h2>
               <p class="subtitle">We develop a roadmap.</p>
-              <p>We make it easy for you to activate the strategy fast and effectively with clear priorities.</p>
+              <p>
+                We make it easy for you to activate the strategy fast and
+                effectively with clear priorities.
+              </p>
             </li>
           </ul>
         </section>
@@ -57,33 +80,48 @@ export default function Home() {
             <li>
               <h3>Hilary Cootes</h3>
               <p class="subtitle">Brand strategist.</p>
-              <p>20 years. 7 agencies. 4 countries.<br/>
-              Large international brands and brands for proud local owners.
+              <p>
+                20 years. 7 agencies. 4 countries.
+                <br />
+                Large international brands and brands for proud local owners.
               </p>
               <a href="https://hilarycootes.com/">hilarycootes.com</a>
             </li>
             <li>
               <h3>Kath Knight</h3>
               <p class="subtitle">Culture strategist.</p>
-              <p>20 years. Ex agency owner.<br/>
-              Government. FMCG. Energy. Insurance. Finance
+              <p>
+                20 years. Ex agency owner.
+                <br />
+                Government. FMCG. Energy. Insurance. Finance
               </p>
               <a href="/">LinkedIn</a>
             </li>
             <li>
               <h2>Together.</h2>
               <ul>
-                <li><a href="/">The Warehouse Group</a></li>
-                <li><a href="/">ACC</a></li>
-                <li><a href="/">IANZ</a></li>
+                <li>
+                  <a href="/">The Warehouse Group</a>
+                </li>
+                <li>
+                  <a href="/">ACC</a>
+                </li>
+                <li>
+                  <a href="/">IANZ</a>
+                </li>
               </ul>
-              <p>Our success is measured in your people’s engagement lift and your overall business performance.</p>
+              <p>
+                Our success is measured in your people’s engagement lift and
+                your overall business performance.
+              </p>
             </li>
           </ul>
         </section>
 
         <section>
-          <h2><a href="/">Contact us →</a></h2>
+          <h2>
+            <a href="/">Contact us →</a>
+          </h2>
         </section>
       </main>
 
@@ -91,5 +129,5 @@ export default function Home() {
         <p>copyright two strategists 2020</p>
       </footer>
     </>
-  )
+  );
 }
